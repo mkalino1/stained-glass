8ada8410e413db931d9cafbfce341e7a5bb04e2b
mkalino1 <mateuszkalin1@gmail.com>
2025-01-08 20:41:09 +0100

    feat: :sparkles: Add shapes rotation and bloated shape

--- components/ArtCanvas.vue
+++ components/ArtCanvas.vue
@@ -1,17 +1,19 @@
 <script setup>
 const shapes = ref([])
+const bloatMode = ref(true)
 function addShape({ target }) {
   shapes.value.push({
     x: target.getBoundingClientRect().x,
     y: target.getBoundingClientRect().y,
-    bold: Math.random() > 0.92,
+    angle: 90 * (rotateCounter.value % 4),
+    path: createPath(target.getBoundingClientRect().x, target.getBoundingClientRect().y, bloatMode.value)
   })
   pushHistory()
 }
-function createPath(x, y, bold) {
+function createPath(x, y, bloat) {
   return `
       M ${x},${y}
-      a 100 100 0 0 ${bold ? 1 : 0} 100 100
+      a 100 100 0 0 ${bloat ? 1 : 0} 100 100
       h -100 Z`
   // d = "M 25 1.5 a 23.5 23.5 0 0 0 -23.5 23.5 h 23.5 Z"
 }
@@ -49,11 +51,12 @@ function handleWheel(event) {
       <rect @click="addShape" v-for="i in 4" class="fixed" width="100" height="100" :x="110 * i" :y="110 * j" :fill="'#666'" />
     </template>
     <path v-for="el in shapes" ref="tmp" fill="#229922" stroke="#123712" stroke-width="5"
-      :d="createPath(el.x, el.y, el.bold)" @contextmenu.prevent="console.log('preventing')"/>
+      :d="el.path" @contextmenu.prevent="console.log('preventing')"
+      :style="{'transform-origin': `${el.x+50}px ${el.y+50}px`, transform: `rotate(${el.angle}deg)`}"/>
   </svg>
-
   <div class="fixed top-3 left-0 right-0 text-center">
     {{ rotateCounter }}
+    <input type="checkbox" v-model="bloatMode"/> 
     <button @click="undoHistory" :disabled="index <= 0" class="mr-2">Undo</button>
     <button @click="redoHistory" :disabled="index >= history.length - 1">Redo</button>
   </div>
