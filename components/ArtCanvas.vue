0ab0d7078b9fac8ad4e5ffb382133105d51e7b35
mkalino1 <mateuszkalin1@gmail.com>
2025-01-09 20:18:22 +0100

    feat: :sparkles: Add shadow shape and types

--- components/ArtCanvas.vue
+++ components/ArtCanvas.vue
@@ -1,16 +1,31 @@
-<script setup>
-const shapes = ref([])
+<script setup lang="ts">
+type Shape = {
+  x: number;
+  y: number;
+  angle: number;
+  path: string;
+  opacity: number;
+};
+
+const shapes = ref<Shape[]>([])
+const shadowShape = ref<Shape | null>()
+const shapesToRender = computed(() => shadowShape.value ? [...shapes.value, shadowShape.value] : shapes.value)
 const bloatMode = ref(true)
-function addShape({ target }) {
-  shapes.value.push({
+
+function addShape({ target }: Event) {
+  shapes.value.push(buildShape(target as HTMLElement))
+  pushHistory()
+}
+function buildShape(target: HTMLElement, shadow = false): Shape {
+  return ({
     x: target.getBoundingClientRect().x,
     y: target.getBoundingClientRect().y,
     angle: 90 * (rotateCounter.value % 4),
-    path: createPath(target.getBoundingClientRect().x, target.getBoundingClientRect().y, bloatMode.value)
+    path: buildPath(target.getBoundingClientRect().x, target.getBoundingClientRect().y, bloatMode.value),
+    opacity: shadow ? 0.3 : 0.8
   })
-  pushHistory()
 }
-function createPath(x, y, bloat) {
+function buildPath(x: number, y: number, bloat: boolean) {
   return `
       M ${x},${y}
       a 100 100 0 0 ${bloat ? 1 : 0} 100 100
@@ -19,7 +34,7 @@ function createPath(x, y, bloat) {
 }
 
 const index = ref(0)
-const history = shallowReactive([[]])
+const history = shallowReactive<Shape[][]>([[]])
 function pushHistory() {
   history.length = ++index.value
   history.push(clone(shapes.value))
@@ -30,33 +45,37 @@ function undoHistory() {
 function redoHistory() {
   shapes.value = clone(history[++index.value])
 }
-function clone(shapes) {
+function clone(shapes: Shape[]) {
   return shapes.map((s) => ({ ...s }))
 }
 
 const rotateCounter = ref(0)
 // TODO: abstract rotating logic to the parent
-function handleWheel(event) {
-  if (event.wheelDeltaY > 0) {
-    rotateCounter.value += 1
-  } else {
-    rotateCounter.value -= 1
+function handleWheel(event: WheelEvent) {
+  rotateCounter.value += Number(event.deltaY < 0)
+  if (shadowShape.value) {
+    shadowShape.value.angle += 90 * (rotateCounter.value + Number(event.deltaY < 0) % 4)
   }
+}
+
+function onMouseover({ target }: Event) {
+  shadowShape.value = buildShape(target as HTMLElement, true)
 }
 </script>
 
 <template>
   <svg class="fixed w-screen h-screen bg-gray-800" @wheel="handleWheel">
     <template v-for="j in 4">
-      <rect @click="addShape" v-for="i in 4" class="fixed" width="100" height="100" :x="110 * i" :y="110 * j" :fill="'#666'" />
+      <rect v-for="i in 4" @click="addShape" @mouseover="onMouseover" class="fixed" width="100" height="100"
+        :x="110 * i" :y="110 * j" :fill="'#666'" />
     </template>
-    <path v-for="el in shapes" ref="tmp" fill="#229922" stroke="#123712" stroke-width="5"
-      :d="el.path" @contextmenu.prevent="console.log('preventing')"
-      :style="{'transform-origin': `${el.x+50}px ${el.y+50}px`, transform: `rotate(${el.angle}deg)`}"/>
+    <path v-for="el in shapesToRender" ref="tmp" fill="#229922" stroke="#123712" stroke-width="5" :d="el.path"
+      @contextmenu.prevent="console.log('preventing')"
+      :style="{ 'transform-origin': `${el.x + 50}px ${el.y + 50}px`, transform: `rotate(${el.angle}deg)`, opacity: el.opacity }" />
   </svg>
   <div class="fixed top-3 left-0 right-0 text-center">
     {{ rotateCounter }}
-    <input type="checkbox" v-model="bloatMode"/> 
+    <input type="checkbox" v-model="bloatMode" />
     <button @click="undoHistory" :disabled="index <= 0" class="mr-2">Undo</button>
     <button @click="redoHistory" :disabled="index >= history.length - 1">Redo</button>
   </div>
