5203e1d9a18aed5207d78bb72de3b690ce6f11ff
mkalino1 <mateuszkalin1@gmail.com>
2025-01-30 21:05:59 +0100

    chore: :heavy_plus_sign: Add nuxt hub for cloudflare deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint", "@pinia/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/eslint",
+    "@pinia/nuxt",
+    "@nuxthub/core",
+  ],
   devtools: { enabled: true },
   compatibilityDate: "2024-11-01",
   components: [
