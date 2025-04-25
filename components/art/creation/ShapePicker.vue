701eb2223a415e77b626d6ae99990212e2481374
Mateusz Kalinowski <mateuszkalin1@gmail.com>
2025-04-25 13:16:50 +0200

    style: :lipstick: Highlight current shape in picker

--- components/art/creation/ShapePicker.vue
+++ components/art/creation/ShapePicker.vue
@@ -1,18 +1,18 @@
 <template>
   <fieldset class="border rounded border-zinc-400 flex gap-4 px-3 py-4">
     <legend class="text-center px-3">Shape</legend>
-    <svg viewBox="0 0 120 120" class="cursor-pointer" @click="shapeName = 'moon'">
-      <Shape :shape="buildDisplayShape('moon')" />
+    <svg viewBox="-2 -2 124 124" class="cursor-pointer" @click="shapeName = 'moon'">
+      <Shape :shape="buildDisplayShape('moon', shapeName == 'moon' ? color : '')" />
     </svg>
-    <svg viewBox="0 0 120 120" class="cursor-pointer" @click="shapeName = 'arc'">
-      <Shape :shape="buildDisplayShape('arc')" />
+    <svg viewBox="-2 -2 124 124" class="cursor-pointer" @click="shapeName = 'arc'">
+      <Shape :shape="buildDisplayShape('arc', shapeName == 'arc' ? color : '')" />
     </svg>
-    <svg viewBox="0 0 120 120" class="cursor-pointer" @click="shapeName = 'marquise'">
-      <Shape :shape="buildDisplayShape('marquise')" />
+    <svg viewBox="-2 -2 124 124" class="cursor-pointer" @click="shapeName = 'marquise'">
+      <Shape :shape="buildDisplayShape('marquise', shapeName == 'marquise' ? color : '')" />
     </svg>
   </fieldset>
 </template>
 
 <script lang="ts" setup>
-const { shapeName } = storeToRefs(useArtControlsStore())
+const { shapeName, color } = storeToRefs(useArtControlsStore())
 </script>
